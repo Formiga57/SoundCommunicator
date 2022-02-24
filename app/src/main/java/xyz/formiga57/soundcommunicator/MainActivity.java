979d46a0066dd2b966502faa3de074633cdff4f8
Vinícius Formigone <formiga5557@gmail.com>
2022-02-23 22:57:57 -0300

    Initial commit

---
+++ app/src/main/java/xyz/formiga57/soundcommunicator/MainActivity.java
@@ -0,0 +1,16 @@
+package xyz.formiga57.soundcommunicator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import xyz.formiga57.soundcommunicator.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
